adeef0b496c77a4df0db58bae01661d047db38a6
Dagmara Grabowska <dagmarag0@gmail.com>
2023-02-28 19:50:33 +0100

    iteration 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import IdCard from './components/IdCard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <IdCard 
+      picture="https://randomuser.me/api/portraits/men/44.jpg"
+      firstName="John"
+      lastName="Doe"
+      gender="male"
+      height={178}
+      birth={("1992-05-11")}
+     />
     </div>
   );
 }
---
+++ src/components/IdCard.jsx
@@ -0,0 +1,17 @@
+function IdCard(props) {
+  const {lastName, firstName, gender, height, birth, picture} = props
+  return (
+    <div>
+      <img src={picture} alt="picture" />
+      <div>
+      <p>First Name: {firstName}</p>
+      <p>Last Name: {lastName}</p>
+      <p>Gender: {gender}</p>
+      <p>Height: {height}</p>
+      <p>Birth: {birth}</p>
+      </div>
+    </div>
+  )
+}
+
+export default IdCard
